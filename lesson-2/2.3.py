21d4b0be1f77322ecfdbad314a94d2f0ff7c3244
ron.est <ronaldbozesteban@gmail.com>
2025-12-25 19:11:30 -0800

    2.3 assignment

---
+++ lesson-2/2.3.py
@@ -0,0 +1,11 @@
+# This first line is provided for you
+
+hrs = input("Enter Hours: ")
+hrs_float = float(hrs)
+
+rate = input("Enter Rate: ")
+rate_float = float(rate)
+
+pay = hrs_float * rate_float
+
+print("Pay:", pay)
